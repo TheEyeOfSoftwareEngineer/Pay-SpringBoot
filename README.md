3f1025789e0dcfebad1b58561a8cbaed0c7408eb
will <u6987162@gmail.com>
2021-03-07 16:42:24 +1100

    project init and clean

---
+++ README.md
@@ -0,0 +1,6 @@
+## README
+
+SpringBoot Project
+- jdk8
+- maven3.6.1
+- IDEA 2019.1.3
---
+++ src/main/java/com/willyao/pay/PayApplication.java
@@ -0,0 +1,14 @@
+package com.willyao.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
